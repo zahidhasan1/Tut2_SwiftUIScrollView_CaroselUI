0c8a49665287cf2ed02d15974b797ee067d4866e
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 13:47:50 +0600

    Initial Commit

---
+++ Tut2_SwiftUIScrollView_CaroselUI/Tut2_SwiftUIScrollView_CaroselUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tut2_SwiftUIScrollView_CaroselUIApp.swift
+//  Tut2_SwiftUIScrollView_CaroselUI
+//
+//  Created by ZEUS on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Tut2_SwiftUIScrollView_CaroselUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
